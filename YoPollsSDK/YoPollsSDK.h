2ed4daac3c32d37f022d7f8a379d09f4235e23f2
Or Arbel <orarbel@gmail.com>
2016-02-04 22:08:41 -0800

    Initial Commit

---
+++ YoPollsSDK/YoPollsSDK.h
@@ -0,0 +1,13 @@
+//
+//  YoPollsSDK.h
+//  YoPollsSDK
+//
+//  Created by mac on 2/4/16.
+//  Copyright © 2016 Life Before Us, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YoPollsSDK : NSObject
+
+@end
---
+++ YoPollsSDK/YoPollsSDK.m
@@ -0,0 +1,13 @@
+//
+//  YoPollsSDK.m
+//  YoPollsSDK
+//
+//  Created by mac on 2/4/16.
+//  Copyright © 2016 Life Before Us, Inc. All rights reserved.
+//
+
+#import "YoPollsSDK.h"
+
+@implementation YoPollsSDK
+
+@end
